ee0b2acaa2b8fab5b5faa89085ea12528a92aadf
ahmedkmadani <ahmedkamal543@gmail.com>
2019-06-18 15:06:15 +0000

    boss hope this work

--- src/ui/Price.js
+++ src/ui/Price.js
@@ -3,7 +3,7 @@ import React  from 'react'
 const Price = props =>(
     <form onSubmit={props.getPrice}>
             {/* <label>Month :</label> */}
-            <input  name='month' type="text" placeholder="Month(Numbers only)"/>
+            <input  name='month' type="text" placeholder="Month (Numbers only)"/>
             {/* <label>Year :</label> */}
             <input  name='year' type="text" placeholder="Year"/>
             <button> Check</button> 
